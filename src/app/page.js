6029f8c74858fc882a5766451142bfec971428c4
Ivi11 <iviherpol11@gmail.com>
2025-07-20 19:40:09 +0200

    .

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import {useState, useEffect} from 'react'
 import Navbar from '@/components/Navbar'
 import HomePage from '@/components/HomePage'
 import Work from '@/components/Work'
-import About from '@/components/About'
+import About from '@/components/about'
 import Footer from '@/components/Footer'
 
 export default function Home() {
